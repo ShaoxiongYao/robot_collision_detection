ae1717deba879973a3c0179c1165edeb30da7dd1
Joao Bimbo <joao.bimbo@iit.it>
2018-05-29 21:24:59 +0200

    add

---
+++ src/robot_collision_detection_node.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Joao Bimbo on 07/12/17.
+//
+
+#include <ros/ros.h>
+#include <robot_collision_detection/CollisionPF.h>
+
+int main(int argc, char** argv) {
+
+    ros::init(argc, argv, "robot_collision_detection");
+    ROS_INFO("CC");
+
+    CollisionPF col;
+    col.run();
+
+    return 0;
+}
